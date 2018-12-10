0c513cbf683ad8baafd7b065c2fcb3941aacdd3a
daniera3 <34807820+daniera3@users.noreply.github.com>
2018-12-10 02:37:16 +0200

    make signin

--- site/site/Controllers/AccController.cs
+++ site/site/Controllers/AccController.cs
@@ -13,56 +13,62 @@ namespace site.Controllers
     {
    
 
-        public ActionResult Submit(Register obj)
+
+        public ActionResult Login(Login obj)
         {
-            if (ModelState.IsValid)
-                return View(obj);
-            return View("Register", obj);
+            return View(obj);
         }
 
-        public ActionResult Login()
-        {
-            return View(new Login());
-        }
         public ActionResult IsRegister(Login obj)
         {
-            
-            if ((obj.user == "37412555" && obj.password == "111aaa") || (obj.user == "11111111" && obj.password == "222bbb") || (obj.user == "22222222" && obj.password == "333ccc") || (obj.user == "" && obj.password == ""))
-            {
-                return View(new Register { User = obj.user });
-            }
-            ViewBag.error = "id/possword no incorrect";
+            Signin d = new Signin();
+            string Message = d.Check(obj);
+            if (Message == "true")
+                return RedirectToAction("Index", "Home");
+            ViewBag.error = Message;
             return View("Login", obj);
 
         }
+
         public ActionResult Register(Register obj)
         {
             if (obj.password == obj.Check && !(obj.password is null))
             {
-                try
+                AddNowUser d = new AddNowUser();
+               string Message = d.NowUser(obj);
+                if (Message == "true")
+                    return RedirectToAction("Login", new Login { user = obj.User });
+                else
                 {
-                    String query = "INSERT INTO [dbo].[user] ([userID], [password]) VALUES (@userID,@password)";
-                    using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Example-of-Microservices\\site\\site\\App_Data\\MyData.mdf;Integrated Security=True"))
-                    using (SqlCommand command = new SqlCommand(query, connection))
-                    {
-                        //a shorter syntax to adding parameters
-                        command.Parameters.AddWithValue("@userID", obj.User);
-
-                        command.Parameters.AddWithValue("@password", obj.password);
-                        //make sure you open and close(after executing) the connection
-                        connection.Open();
-                        command.ExecuteNonQuery();
-                        connection.Close();
-                    }
-                
-
-                return RedirectToAction("Index","Home");
-                 }
-                catch (SqlException ex)
-                {
-                    ViewBag.error = ex.Message;
+                    ViewBag.error = Message;
                     return View(new Register());
                 }
+                /*
+                 *try
+                 {
+                     String query = "INSERT INTO [dbo].[user] ([userID], [password]) VALUES (@userID,@password)";
+                     using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Example-of-Microservices\\site\\site\\App_Data\\MyData.mdf;Integrated Security=True"))
+                     using (SqlCommand command = new SqlCommand(query, connection))
+                     {
+                         //a shorter syntax to adding parameters
+                         command.Parameters.AddWithValue("@userID", obj.User);
+
+                         command.Parameters.AddWithValue("@password", obj.password);
+                         //make sure you open and close(after executing) the connection
+                         connection.Open();
+                         command.ExecuteNonQuery();
+                         connection.Close();
+                     }
+
+
+                 return RedirectToAction("Index","Home");
+                  }
+                 catch (SqlException ex)
+                 {
+                     ViewBag.error = ex.Message;
+                     return View(new Register());
+                 }
+                 */
             }
             else if(obj.password != obj.Check)
             {
---
+++ site/site/Models/AddNowUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace site.Models
+{
+    public class AddNowUser
+    {
+
+        public string NowUser(Register obj)
+        {
+            try
+            {
+                String query = "INSERT INTO [dbo].[user] ([userID], [password]) VALUES (@userID,@password)";
+                using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Example-of-Microservices\\site\\site\\App_Data\\MyData.mdf;Integrated Security=True"))
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    //a shorter syntax to adding parameters
+                    command.Parameters.AddWithValue("@userID", obj.User);
+
+                    command.Parameters.AddWithValue("@password", obj.password);
+                    //make sure you open and close(after executing) the connection
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+
+                return "true";
+            }
+            catch (SqlException ex)
+            {
+                return ex.Message;
+              
+            }
+        }
+    }
+}
