e0a9a3deeda1826fd19416d8d53ce6589fea0de6
daniera3 <34807820+daniera3@users.noreply.github.com>
2018-12-14 15:23:42 +0200

    1

--- WebApplication1/WebApplication1/App_Start/Startup.Auth.cs
+++ WebApplication1/WebApplication1/App_Start/Startup.Auth.cs
@@ -55,8 +55,8 @@ namespace WebApplication1
             //   consumerSecret: "");
 
             //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+              // appId: "",
+              // appSecret: "");
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
