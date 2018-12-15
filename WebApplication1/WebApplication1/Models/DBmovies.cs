ca3e82c6def98edc517657a33628ecba7445643d
daniera3 <34807820+daniera3@users.noreply.github.com>
2018-12-15 19:38:19 +0200

    1

---
+++ WebApplication1/WebApplication1/Models/DBmovies.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApplication1.Models
+{
+    public class DBmovies
+    {
+        public SqlDataReader Reader { get; set; }
+        public string getmovies()
+        {
+            try
+            {
+
+                string sqlCon = "Data Source=(localdb)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|aspnet-WebApplication1-20181211112737.mdf;Integrated Security=True";
+                string query = "SELECT * FROM [dbo].[Movie]";
+                //using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\nicolro1\\Desktop\\Example-of-Microservices--master\\site\\site\\App_Data\\MyData.mdf;Integrated Security=True"))
+                using (SqlConnection connection = new SqlConnection(sqlCon))
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    //a shorter syntax to adding parameters
+                    //make sure you open and close(after executing) the connection
+                    try
+                    {
+                        connection.Open();
+                    }
+                    catch (Exception ex)
+                    {
+                        return ex.Message;
+
+                    }
+                    Reader = command.ExecuteReader();
+                    return "true";
+                }
+            }
+            catch (SqlException ex)
+            {
+                return ex.Message;
+
+            }
+        }
+    }
+}
