762755778cbbce785cb9328c4ba79ece0c29d590
denis <ddenis1994@gmail.com>
2018-12-20 19:07:22 +0200

    working runnuble java file

--- javaSqlRetrive/src/Retriver.java
+++ javaSqlRetrive/src/Retriver.java
@@ -2,8 +2,11 @@
 import java.sql.Connection;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.sql.DriverManager;
@@ -13,31 +16,60 @@ import java.sql.Statement;
 
 public class Retriver {
 
-	public static void main(String[] args) {
+	@SuppressWarnings("null")
+	public static void main(String[] args) throws IOException {
 		File config=new File("config");
+		FileWriter log=null;
+		try {
+			log=new FileWriter("log.txt");
+		} catch (IOException e2) {
+			log.write("cannot write to the log file");
+		}
 		Scanner sc=null;
 		try {
 			sc = new Scanner(config);
 		} catch (FileNotFoundException e1) {
-			System.out.println("cannot open config  file");
+			log.write("cannot open config  file");
 		}
-
 		  // set the connection string in the connection object
         String connectionUrl =sc.nextLine();
         try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
             String SQL = "SELECT * FROM [dbo].[Movie]";
             ResultSet rs = stmt.executeQuery(SQL);
-            //run on the data from the sql server and extarct
+            //run on the data from the sql server and extract
+            JSONArray jsonArray = new JSONArray();
             while (rs.next()) {
-            	JSONObject result = new JSONObject();
-            	
-                System.out.println(rs.getString("FirstName") + " " + rs.getString("LastName"));
+                int total_rows = rs.getMetaData().getColumnCount();
+                for (int i = 0; i < total_rows; i++) {
+                    JSONObject obj = new JSONObject();
+                    obj.put(rs.getMetaData().getColumnLabel(i + 1)
+                            .toLowerCase(), rs.getObject(i + 1));
+                    jsonArray.put(obj);
+                }
             }
+            FileWriter result=null;
+            try {
+				result=new FileWriter("result.json");
+			} catch (IOException e) {
+				//try to create the output file
+				log.write("cannot create outputfile");
+			}
+            try {
+				result.write(jsonArray.toString());
+				result.close();
+			} catch (IOException e) {
+				
+				log.write("eror : cannot convert json to string");
+			}
+            
         }
         // Handle any errors that may have occurred.
         catch (SQLException e) {
-            System.out.println("Eror: cannot connect to the server");
+        	
+        	log.write("Eror: cannot connect to the server");
         }
+        
+        log.close();
 	}
 
 }
