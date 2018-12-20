0a52b8a74390247aeae3e87fca61dacc26680d85
denis <ddenis1994@gmail.com>
2018-12-20 16:43:01 +0200

    added the matadata

--- javaSqlRetrive/src/Retriver.java
+++ javaSqlRetrive/src/Retriver.java
@@ -1,12 +1,18 @@
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Retriver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		  // Create a variable for the connection string.
-        String connectionUrl = "jdbc:sqlserver://<server>:<port>;databaseName=AdventureWorks;user=<user>;password=<password>";
-
+        String connectionUrl = "jdbc:sqlserver://DESKTOP-SJGIRIB:<port>;databaseName=AdventureWorks;user=<user>;password=<password>";
+        //Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=C:\Users\97254\Documents\microserviceProject\Example-of-Microservices\WebApplication1\WebApplication1\App_Data\aspnet-WebApplication1-20181211112737.mdf;Initial Catalog=aspnet-WebApplication1-20181211112737;Integrated Security=True
         try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
             String SQL = "SELECT TOP 10 * FROM Person.Contact";
             ResultSet rs = stmt.executeQuery(SQL);
@@ -20,7 +26,7 @@ public class Retriver {
         catch (SQLException e) {
             e.printStackTrace();
         }
-
+        System.out.println("dear god");
 	}
 
 }
